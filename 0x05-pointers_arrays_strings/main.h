892be4c5f9eabf2c7ecc27f05fbcfadd7ae7cc65
Babalwa J Mazingisa <mazingisabj@aol.com>
2023-02-28 17:53:23 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-cd main.h >
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -16,7 +15,9 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _atoi(char *s);
+101-crackme.
 
 #endif
-
 
